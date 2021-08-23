d77b2c56cd3144cad63266f89bb63ece27e82f42
Kaylane-Guarino <guarinokaah@gmail.com>
2021-08-23 11:29:39 -0300

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,7 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-
-    res.send("Hello World Express");
-});
+const app = require("./app");
 
 app.listen(3333, () => {
 
-    console.log("Servidor rodando na porta 3333");
+    console.log(`Servidor rodando na porta ${3333}`);
 
-})
+});
